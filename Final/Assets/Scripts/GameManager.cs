884524afad6c653f02aa04deff790cf0b376d954
Kevin Andrade <kandrad2@binghamton.edu>
2019-05-13 23:57:40 -0400

    added more objects

---
+++ Final/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager singleton; //object only exists once
+
+	private GroundPiece[] allGroundPieces;
+
+
+	// Use this for initialization
+	void Start () {
+			SettupNewLevel();
+	}
+	private void SettupNewLevel()
+	{
+		allGroundPieces = FindObjectsOfType<GroundPiece>();	//im going to check in all the scene if theres a type groundpiece
+	}
+}
