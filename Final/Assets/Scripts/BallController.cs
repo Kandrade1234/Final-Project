fcc1c76cabe79ffb6135c3da9d02ad0f48c16481
Kevin Andrade <kandrad2@binghamton.edu>
2019-05-14 01:05:59 -0400

    finished level 1

--- Final/Assets/Scripts/BallController.cs
+++ Final/Assets/Scripts/BallController.cs
@@ -4,13 +4,103 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Rigidbody rb;
+	public float speed = 15; // speed of our Ball
+
+
+	private bool isMoving;
+	private Vector3 travelDirection;
+	private Vector3 nextCollisionPosition;
+	public int minSwipeRecognition = 500;
+	private Vector2 swipePositionLastFrame;
+	private Vector2 swipePositionCurrentFrame;
+	private Vector2 currentSwipe;
+
+
+
+
+	private Color solveColor;
+
+	private void Start()
+	{
+		solveColor = Random.ColorHSV(0.5f, 1);
+		GetComponent<MeshRenderer>().material.color = solveColor;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void FixedUpdate()
+	{
+		if(isMoving)
+		{
+			rb.velocity = speed * travelDirection;
+		}
+
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position - (Vector3.up / 2), 0.05f);
+		int i  = 0;
+		while(i < hitColliders.Length)
+		{
+			GroundPiece ground = hitColliders[i].transform.GetComponent<GroundPiece>();
+			if(ground && !ground.isColored)
+			{
+				ground.ChangeColor(solveColor);
+			}
+			i++;
+		}
+
+		if(nextCollisionPosition != Vector3.zero)
+		{
+			if(Vector3.Distance(transform.position, nextCollisionPosition) < 1)
+			{
+				isMoving = false;
+				travelDirection = Vector3.zero;	///shouldnt be moving anymore
+				nextCollisionPosition  = Vector3.zero;
+			}
+		}
+		if(isMoving)
+			return;
+		if(Input.GetMouseButton(0))	//either with finger or mouse on screen
+		{
+			swipePositionCurrentFrame = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+			if(swipePositionLastFrame != Vector2.zero)
+			{
+					currentSwipe = swipePositionCurrentFrame - swipePositionLastFrame;
+					if(currentSwipe.sqrMagnitude < minSwipeRecognition)
+					{
+						return;
+					}
+					currentSwipe.Normalize();	//getting the direction not the distance	up left right down etc.
+					if(currentSwipe.x > -0.5f && currentSwipe.x < 0.5)
+					{
+						//GO UP/DOWN
+						SetDestination(currentSwipe.y > 0 ? Vector3.forward : Vector3.back);
+					}
+					if(currentSwipe.y > -0.5f && currentSwipe.y < 0.5)
+					{
+						//Go left/right
+						SetDestination(currentSwipe.x > 0 ? Vector3.right: Vector3.left);
+					}
+
+			}
+			swipePositionLastFrame = swipePositionCurrentFrame;
+		}
+		if(Input.GetMouseButtonUp(0))
+		{
+			swipePositionLastFrame = Vector2.zero;
+			currentSwipe = Vector2.zero;
+		}
+	}
+
+
+
+
+	private void SetDestination(Vector3 direction)
+	{
+		travelDirection = direction;
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, direction, out hit, 100f))
+		{
+			nextCollisionPosition = hit.point;
+		}
+		isMoving = true;
 	}
 }
